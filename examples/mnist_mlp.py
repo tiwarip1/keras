df23a603d5e0bd9c11c5fdcac49540a236da69c0
HerpDerp <herpitehderpo@gmail.com>
2018-01-19 19:38:01 -0500

    Expanded on examples for deep learning and tried to implement those ideas to stocks unsuccessfully

--- examples/mnist_mlp.py
+++ examples/mnist_mlp.py
@@ -15,7 +15,7 @@ from keras.optimizers import RMSprop
 
 batch_size = 128
 num_classes = 10
-epochs = 20
+epochs = 10
 
 # the data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
